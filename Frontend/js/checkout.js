915c3e56073cea0992bf25b4c03a5b7e8da86519
Ayush Joshi <studyingayush8@gmail.com>
2026-08-31 13:29:28 +0530

    Update checkout.js

--- Frontend/js/checkout.js
+++ Frontend/js/checkout.js
@@ -54,7 +54,7 @@ document.getElementById("checkoutForm").addEventListener("submit", async (e) => 
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/orders", {
+        const response = await fetch("https://run-fold-01.vercel.app/api/orders", {
 
             method: "POST",
 
